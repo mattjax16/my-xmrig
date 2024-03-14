23d01ac4fdff81d3369206212a0fdb40ee79f846
Matt Bass <mattjax16@gmail.com>
2024-03-14 14:01:18 -0400

    changed donation level to 0 for now sorry dude im broke recently graduated from college

--- src/donate.h
+++ src/donate.h
@@ -1,6 +1,7 @@
 /* XMRig
  * Copyright (c) 2018-2022 SChernykh   <https://github.com/SChernykh>
- * Copyright (c) 2016-2022 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright (c) 2016-2022 XMRig       <https://github.com/xmrig>,
+ * <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -19,26 +20,29 @@
 #ifndef XMRIG_DONATE_H
 #define XMRIG_DONATE_H
 
-
 /*
  * Dev donation.
  *
- * Percentage of your hashing power that you want to donate to the developer can be 0% but supports XMRig Development.
+ * Percentage of your hashing power that you want to donate to the developer can
+ * be 0% but supports XMRig Development.
  *
  * Example of how it works for the setting of 1%:
- * Your miner will mine into your usual pool for a random time (in a range from 49.5 to 148.5 minutes),
- * then switch to the developer's pool for 1 minute, then switch again to your pool for 99 minutes
- * and then switch again to developer's pool for 1 minute; these rounds will continue until the miner stops.
+ * Your miner will mine into your usual pool for a random time (in a range
+ * from 49.5 to 148.5 minutes), then switch to the developer's pool for 1
+ * minute, then switch again to your pool for 99 minutes and then switch again
+ * to developer's pool for 1 minute; these rounds will continue until the miner
+ * stops.
  *
  * Randomised only on the first round to prevent waves on the donation pool.
  *
- * Switching is instant and only happens after a successful connection, so you never lose any hashes.
+ * Switching is instant and only happens after a successful connection, so you
+ * never lose any hashes.
  *
- * If you plan on changing donations to 0%, please consider making a one-off donation to my wallet:
- * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
+ * If you plan on changing donations to 0%, please consider making a one-off
+ * donation to my wallet: XMR:
+ * 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
  */
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
-
+constexpr const int kDefaultDonateLevel = 0;
+constexpr const int kMinimumDonateLevel = 0;
 
 #endif // XMRIG_DONATE_H
